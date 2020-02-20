48d46bb7f0a59dabd0ad34b5b3901aa308538be5
FauzanKatil <FauzanKatil@github.com>
2020-02-20 09:54:09 +0700

    afauzan

---
+++ app/src/main/java/com/arryndevteam/myfirstproject/RelativeAct.java
@@ -0,0 +1,14 @@
+package com.arryndevteam.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
